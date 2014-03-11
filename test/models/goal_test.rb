6a4b1673c4c966b37cdf0137b35bf57b50bb996e
Jose Villa <milquinto@gmail.com>
2014-03-10 18:06:07 -0700

    unit tests

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @to_buy = goals :buy
+    @to_lose = goals :lose
+    @user = users :pete
+    @cat = goal_categories :sports
+    @type = goal_types :progressive
+  end
+
+  test "creating a goal" do
+    goal_params = { user_id: @user.id, name: "Climb mountain", description: "Go higher each day.", image: "image.url", goal_category_id: @cat.id, goal_type_id: @type.id }
+    assert Goal.create( goal_params )
+  end
+
+  test "followers" do
+    assert_instance_of Array, @to_buy.followers
+    assert_equal @to_buy.followers, 2
+  end
+
+  test "comments" do
+    assert @to_buy.comments
+  end
+
+  test "info" do
+    assert @to_buy.description
+    assert @to_buy.results
+    assert @to_buy.rate
+  end
+
+  test "progress" do
+    @follower = @to_buy.followers.first
+    assert @to_buy.progress( @follower )
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @john = users :john
+    @pete = users :pete
+  end
+
+  test "email is unique" do
+    @john.email = @pete.email
+    assert_not @john.save
+  end
+
+  test "register" do
+    user_hash = { name: "Doo", lastname: "Foo", email: "new@user.com"  }
+    new_user = User.create user_hash
+    assert_instance_of User, new_user
+  end
+
+  test "has many goals" do
+    pete_count = 2
+    assert_equal UserGoal.where( user: @pete ).count, pete_count
+    assert_equal @pete.goals.count, pete_count
+
+  end
+
+  test "finding by email" do
+    found = User.find_by_email @john.email
+    assert_equal found, @john
+  end
+
 end
