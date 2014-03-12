276769c6dc543c872b7d42b9a1a86b2a298fa8a7
mitzy <tlamy23@hotmail.com>
2014-03-12 12:52:36 -0700

    fix column goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,11 @@ class Goal < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :goal_category
 	belongs_to :goal_type
+
+	after_create :create_usergoal
+
+	private
+		def create_usergoal
+			ug = UserGoal.create(user_id: self.user_id, goal_id: self.id, state: "0")
+		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	validates_uniqueness_of :email, :uid
+	has_many :user_goals
+	def goals
+		UserGoal.where(user_id: self.id)
+	end
 end
