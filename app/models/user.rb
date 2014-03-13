463b07b20cd23a927dafa2fffac174cb85dd7f68
Jose Villa <milquinto@gmail.com>
2014-03-13 16:43:45 -0700

    Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ActiveRecord::Base
-  validates_uniqueness_of :email, :uid
   has_many :user_goals
+
+  has_many :friendships
+  has_many :_friends, through: :friendships, foreign_key: "friend_id"
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  validates_uniqueness_of :email, :uid
+
   def goals
     UserGoal.where(user_id: self.id)
+  end
+
+  def friends
+    self._friends + self.inverse_friends
   end
 end
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @pete = users :pete
+    @ron = users :ron
+  end
+
+  test "a friend" do
+    assert @pete.friends.find @ron
+  end
+
+  test "counting friends" do
+    assert @pete.friends.count, 2
+  end
+
+  test "an inverse friendship" do
+    assert @ron.inverse_friends.find @pete
+  end
+
+  test "a friend reversed" do
+    assert @ron.friends.find @pete
+  end
 end
