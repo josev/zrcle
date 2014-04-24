cd1322426b12c5081de156e4613fcd4973922091
mitzy <tlamy23@hotmail.com>
2014-04-24 16:30:25 -0700

    add goal attribute with the name steps, return the count of alls step from that goal

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,11 +1,15 @@
 class GoalSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :goal_category_id, :expected_result, :goal_type_id, 
-  :users, :friends, :progress, :current_step, :state
+  :state,:users, :friends, :progress, :current_step, :steps
 
   #has_many :users, through: :user_goals
 
   def users
   	object.users.count
+  end
+
+  def steps
+    GoalStep.where(goal_id: object.id).count
   end
 
   def include_friends?
